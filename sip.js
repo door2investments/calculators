dc4800ac3379a19c8aed16cc4da3a0b2f7da0e64
door2investments <door2investments@gmail.com>
2026-01-02 13:48:03 +0530

    Update sip.js

--- sip.js
+++ sip.js
@@ -1,1 +1,33 @@
+let chart;
+
+function calculateSIP() {
+  let P = +sip.value;
+  let r = rate.value / 12 / 100;
+  let n = years.value * 12;
+
+  let fv = P * ((Math.pow(1+r,n)-1)/r) * (1+r);
+  let invested = P * n;
+  let earned = fv - invested;
+
+  investedEl = invested.toLocaleString();
+  earnedEl = earned.toLocaleString();
+  totalEl = Math.round(fv).toLocaleString();
+
+  invested.innerHTML = investedEl;
+  earned.innerHTML = earnedEl;
+  total.innerHTML = totalEl;
+
+  if(chart) chart.destroy();
+
+  chart = new Chart(sipChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Invested', 'Earned'],
+      datasets: [{
+        data: [invested, earned],
+        backgroundColor: ['#94a3b8','#0f2a44']
+      }]
+    }
+  });
+}
 
