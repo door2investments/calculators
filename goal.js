28a0810dc631f85bcd61f43da78e1358a527eb28
door2investments <door2investments@gmail.com>
2026-01-02 13:48:57 +0530

    Update goal.js

--- goal.js
+++ goal.js
@@ -1,1 +1,14 @@
+function calculateGoal() {
+  let G = +goal.value;
+  let y = +years.value;
+  let inf = +inflation.value/100;
+  let r = +return.value/100/12;
+  let n = y*12;
+
+  let futureGoal = G * Math.pow(1+inf, y);
+  let sip = futureGoal * r / ((Math.pow(1+r,n)-1)*(1+r));
+
+  future.innerHTML = "₹ " + Math.round(futureGoal).toLocaleString();
+  sipreq.innerHTML = "₹ " + Math.round(sip).toLocaleString();
+}
 
