a853febc20442983061c5cd9dd8a447dce2929bb
door2investments <door2investments@gmail.com>
2026-01-02 14:59:42 +0530

    Update goal.js

--- goal.js
+++ goal.js
@@ -10,11 +10,42 @@ function calculateGoal() {
 
   future.innerHTML = "₹ " + Math.round(futureGoal).toLocaleString();
   sipreq.innerHTML = "₹ " + Math.round(sip).toLocaleString();
+  showPopup(
+  "Hi, I used your goal planning calculator and would like help in structuring the right SIP to achieve my goal. Please guide me."
+);
+
 }
 
 // Run default calculation on page load
 window.addEventListener("DOMContentLoaded", () => {
   calculateGoal();
 });
+
+let popupShown = sessionStorage.getItem("advisorPopupShown");
+
+function showPopup(prefilledMessage) {
+  if (popupShown) return;
+
+  const phone = "919390250541"; // your WhatsApp number
+  const encodedMsg = encodeURIComponent(prefilledMessage);
+
+  document.getElementById("whatsappLink").href =
+    `https://wa.me/${phone}?text=${encodedMsg}`;
+
+  document.getElementById("advisorPopup").style.display = "flex";
+  sessionStorage.setItem("advisorPopupShown", "true");
+}
+
+function closePopup() {
+  document.getElementById("advisorPopup").style.display = "none";
+}
+
+setTimeout(() => {
+  showPopup(
+    "Hi, I just used your investment calculator and would like guidance on achieving my goal. Please help me with the right investment strategy."
+  );
+}, 30000);
+
+
 
 
